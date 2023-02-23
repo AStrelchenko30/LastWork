8ad3d349ee94f5abd934e2235a1877c006de0eb5
V-I-K-I <anystrelchenko@gmail.com>
2023-02-23 10:54:33 +0300

    1-я дз

--- src/main/java/ru/skypro/homework/dto/AdsDto.java
+++ src/main/java/ru/skypro/homework/dto/AdsDto.java
@@ -2,13 +2,12 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
-import java.util.Arrays;
 
 @Data
 public class AdsDto {
 
     private Integer author;
-    private Arrays image;
+    private String image;
     private Integer pk;
     private Integer price;
     private String title;
