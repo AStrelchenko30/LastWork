736e64070c51ed3d142aa458e9512890dfbc04d9
V-I-K-I <anystrelchenko@gmail.com>
2023-03-07 21:42:12 +0300

    2-я дз мапперы

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.Ads;
 
+import java.util.Collection;
+
 
 @Repository
 public interface AdsRepository extends JpaRepository<Ads, Long> {
 
     void deleteAllById(Long adsId);
+
+    Collection<Ads> findAllByTitle(String title);
 }
